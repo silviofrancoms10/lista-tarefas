cbda6431e5f016e9f197a770c7e1881f0b27a619
silviofrancoms10 <silviofrancoms@gmail.com>
2025-11-15 00:14:57 -0400

    Components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import Main from "./components/Main";
+import './App.css'
+
+export default function App()  {
+  return (
+    <>
+      <Main />
+      <h1> Hello World!</h1>
+      <p>Lorem</p>
+    </>
+  );
+
+}
