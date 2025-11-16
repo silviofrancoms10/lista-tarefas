165f4c04c5e99341afa65341f25ff51db01bcd22
silviofrancoms10 <silviofrancoms@gmail.com>
2025-11-16 13:30:59 -0400

    Editando tarefas

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,26 +8,41 @@ export default class Main extends Component {
   state = {
     novaTarefa: '',
     tarefas: [],
+    index: -1,
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
-    const { tarefas } = this.state;
+    const { tarefas, index } = this.state;
     let { novaTarefa } = this.state;
     novaTarefa = novaTarefa.trim();
 
     if (tarefas.indexOf(novaTarefa) !== -1) return;
 
     const novasTarefas = [...tarefas];
-    this.setState({
-      tarefas: [...novasTarefas, novaTarefa],
-    });
+
+    if (index === -1) {
+      this.setState({
+        tarefas: [...novasTarefas, novaTarefa],
+        novaTarefa: '',
+      });
+    } else {
+      novasTarefas[index] = novaTarefa
+
+      this.setState({
+        tarefas: [...novasTarefas],
+        index: -1,
+      })
+    }
   }
 
   handleChange = (e) => this.setState({ novaTarefa: e.target.value, })
 
   handleEdit = (e, index) => {
-    console.log('Edit', index)
+    this.setState({
+      novaTarefa: this.state.tarefas[index],
+      index,
+    });
   }
 
   handleDelete = (e, index) => {
